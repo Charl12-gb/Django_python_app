d0fd67194d018dc04073cc4b29e0488bdf5b93ee
Charles GBOYOU <gboyoucharles22@gmail.com>
2022-10-26 00:29:57 +0100

    Models article

--- my_blog/blog/models.py
+++ my_blog/blog/models.py
@@ -1,3 +1,13 @@
+from distutils.command.upload import upload
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    price = models.IntegerField()
+    desc = models.TextField()
+    image = models.ImageField(upload_to='images')
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) :
+        return self.title
